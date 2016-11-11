1b71af6d8dcb47c9264b4734e7df7da6cf3e04e3
optmist2309 <harishtiwary46@gmail.com>
2016-11-12 00:43:48 +0530

    method overloading complete

---
+++ Methodoverload2.java
@@ -0,0 +1,23 @@
+class Methodoverload2  {
+	void sum(int a , int b){
+
+System.out.println(a+b);
+
+	}
+	
+	void sum (double a , double b){
+
+		System.out.println(a+b);
+
+
+	}
+public static void main(String[] args) {
+	Methodoverload2 m = new Methodoverload2();
+	m.sum(10,20);
+	m.sum(12.5,2.5);
+
+
+}
+
+}
+
